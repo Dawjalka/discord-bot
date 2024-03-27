0757d73aeaf4b300ba982608867521f9b9f2e03a
Dawjalka <162482781+Dawjalka@users.noreply.github.com>
2024-03-27 18:29:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
-from settings import settings
+import random
+from settings import token
 from bot_logic import gen_pass
 
 # Zmienna intencje przechowuje uprawnienia bota
@@ -20,14 +21,22 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send("Cześć!")
     elif message.content.startswith('$bye'):
-        await message.channel.send("\\U0001f642")
+        await message.channel.send("Pa!")
     elif message.content.startswith('$password'):
         await message.channel.send(gen_pass(10))
+    elif message.content.startswith('$rzut monetą'):
+        coin = random.randint(1,2)
+        if coin == 1:
+            await message.channel.send("Orzeł")
+        elif coin == 2:
+            await message.channel.send("Reszka")
+    elif message.content.startswith('$rzut kością'):
+        dice = random.randint(1,6)
+        await message.channel.send(dice)
+    elif message.content.startswith('$liczba od 1 do 10'):
+        number = random.randint(1,10)
+        await message.channel.send(number)
     else:
         await message.channel.send(message.content)
 
-@client.event
-async def on_ready():
-    await client.change_presence(status=discord.Status.Online)
-
-client.run(settings['TOKEN'])
+client.run(token['TOKEN'])
